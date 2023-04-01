833a0bc592d22e9f2cd3da2ff975aee9637d9de8
chamsou123 <agounichams1@gmail.com>
2023-04-01 10:03:25 +0100

    feat(graphQl): add GraphQl module

---
+++ src/modules/common/graphql/graphql.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: false,
+      // @ts-ignore
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+  ],
+})
+export class GraphqlApiModule {}
