79fd3141e9f48e87de61acda384492329312ea79
chamsou123 <agounichams1@gmail.com>
2023-04-02 20:28:18 +0100

    feat(errors): add INVALID_CREDENTIALS error constant

--- src/errors/errors.constants.ts
+++ src/errors/errors.constants.ts
@@ -1,3 +1,5 @@
 export const USER_NOT_FOUND = '404001: USER NOT FOUND';
 export const CATEGORY_NOT_FOUND = '404002: Category Not Found';
 export const TODO_NOT_FOUND = '404003: TODO Not Found';
+
+export const INVALID_CREDENTIALS = '401001: Invalid credentials';
