8371750de6ff4e588972019ea0db5c09668fae43
chamsou123 <agounichams1@gmail.com>
2023-04-02 21:31:35 +0100

    feat(todo): add authentication in resolver

--- src/modules/todo/todo.resolver.ts
+++ src/modules/todo/todo.resolver.ts
@@ -1,13 +1,16 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
 
 import { Todo } from './entities';
 import { CreateTodoDto, FilterTodoDto, UpdateTodoDto } from './dto';
 import { TodoService } from './todo.service';
+import { JwtAuthGuard } from '../auth/guards';
 
 @Resolver()
 export class TodoResolver {
   constructor(private readonly todoService: TodoService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Query(() => [Todo], { name: 'todos' })
   async todos(
     @Args('filterTodoInput', { nullable: true }) filterTodoInput: FilterTodoDto,
@@ -15,11 +18,13 @@ export class TodoResolver {
     return await this.todoService.todos(filterTodoInput);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Query(() => Todo, { name: 'todo' })
   async todo(@Args('id') id: number): Promise<Todo> {
     return await this.todoService.todo(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Mutation(() => Todo, { name: 'createTodo' })
   async create(
     @Args('createTodoInput') createTodoInput: CreateTodoDto,
@@ -27,6 +32,7 @@ export class TodoResolver {
     return await this.todoService.create(createTodoInput);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Mutation(() => Todo, { name: 'updateTodo' })
   async update(
     @Args('updateTodoInput') updateTodoInput: UpdateTodoDto,
@@ -34,6 +40,7 @@ export class TodoResolver {
     return await this.todoService.update(updateTodoInput);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Mutation(() => Boolean, { name: 'deleteTodo' })
   async delete(@Args('id') id: number): Promise<boolean> {
     return await this.todoService.delete(id);
