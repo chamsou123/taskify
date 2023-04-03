cdcee9e9bfcf90e9379b25d364d3a34f8445c7ef
chamsou123 <agounichams1@gmail.com>
2023-04-03 01:10:13 +0100

    feat(category): add dto validators

--- src/modules/category/dto/filter-category.dto.ts
+++ src/modules/category/dto/filter-category.dto.ts
@@ -1,9 +1,12 @@
 import { Field, InputType, PartialType } from '@nestjs/graphql';
 
 import { CreateCategoryDto } from './create-category.dto';
+import { IsNumber, IsOptional } from 'class-validator';
 
 @InputType()
 export class FilterCategoryDto extends PartialType(CreateCategoryDto) {
   @Field(() => Number, { nullable: true })
+  @IsNumber()
+  @IsOptional()
   id?: number;
 }
